da86b37e7956f5b6f1249195282572c4cb0ef9fe
alimvleha <alimovleha@gmail.com>
2020-01-23 10:55:11 +0400

    Добавил пример с переменными

--- send/src/main/java/ru/qa/qa/send/MyProgramm.java
+++ send/src/main/java/ru/qa/qa/send/MyProgramm.java
@@ -2,6 +2,10 @@ package ru.qa.qa.send;
 
 public class MyProgramm {
   public static void main(String[] args) {
-    System.out.println("Privit!Privit!Privit!Privit!");
+    System.out.println("Hello," + "man!");
+
+    int l = 12;
+    int s = l * l;
+    System.out.println("Пложадь квадрата со стороной " + l + " = " + s);
   }
 }
