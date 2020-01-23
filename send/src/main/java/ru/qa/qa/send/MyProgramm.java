ef796a21921661296f389bd33deab1826d5e21e9
alimvleha <alimovleha@gmail.com>
2020-01-23 13:54:43 +0400

    добавил несколько функций

--- send/src/main/java/ru/qa/qa/send/MyProgramm.java
+++ send/src/main/java/ru/qa/qa/send/MyProgramm.java
@@ -2,10 +2,26 @@ package ru.qa.qa.send;
 
 public class MyProgramm {
   public static void main(String[] args) {
-    System.out.println("Hello," + "man!");
-
-    int l = 12;
-    int s = l * l;
-    System.out.println("Пложадь квадрата со стороной " + l + " = " + s);
+    hello("world");
+    hello("Aleksey Alimov");
+    double l = 6;
+    System.out.println("Площадь квадрата " + l + " = " + area(l));
+    double a = 4;
+    double b = 6;
+    System.out.println("Площадь треугольника " + a +" и " + b + " = " + area(a, b));
   }
+
+  public static void hello(String somebody) {
+
+    System.out.println("Hello," + somebody + "!");
+  }
+
+  public static double area(double len) {
+    return len * len;
+  }
+
+  public static double area(double a, double b){
+    return a * b;
+  }
+
 }
