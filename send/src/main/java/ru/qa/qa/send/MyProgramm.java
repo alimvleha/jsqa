cd77bcba8e81919d18c7629955258a655afbdc38
alimvleha <alimovleha@gmail.com>
2020-11-06 10:31:18 +0400

    commit 06/11/2020

--- send/src/main/java/ru/qa/qa/send/MyProgramm.java
+++ send/src/main/java/ru/qa/qa/send/MyProgramm.java
@@ -2,13 +2,14 @@ package ru.qa.qa.send;
 
 public class MyProgramm {
   public static void main(String[] args) {
-    hello("world");
+    hello("world ey");
     hello("Aleksey Alimov");
-    double l = 6;
-    System.out.println("Площадь квадрата " + l + " = " + area(l));
-    double a = 4;
-    double b = 6;
-    System.out.println("Площадь треугольника " + a +" и " + b + " = " + area(a, b));
+
+    Square s = new Square(5);
+    System.out.println("Площадь квадрата " + s.l + " = " + area(s));
+
+    Rectangle r = new Rectangle(4,6);
+    System.out.println("Площадь треугольника " + r.a + " и " + r.b + " = " + area(r));
   }
 
   public static void hello(String somebody) {
@@ -16,12 +17,12 @@ public class MyProgramm {
     System.out.println("Hello," + somebody + "!");
   }
 
-  public static double area(double len) {
-    return len * len;
+  public static double area(Square s) {
+    return s.l * s.l;
   }
 
-  public static double area(double a, double b){
-    return a * b;
+  public static double area(Rectangle r) {
+    return r.a * r.b;
   }
 
 }
---
+++ send/src/main/java/ru/qa/qa/send/Square.java
@@ -0,0 +1,11 @@
+package ru.qa.qa.send;
+
+public class Square {
+  public double l;
+
+  public Square(double l) {
+    this.l = l;
+  }
+}
+
+
